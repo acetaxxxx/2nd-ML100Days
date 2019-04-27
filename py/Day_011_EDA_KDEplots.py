8ea50a69a17966a09bf601d67c044f8c38416d14
Home Windows <acetaxxxx@gmail.com>
2019-04-27 21:32:26 +0800

    HW11

---
+++ py/Day_011_EDA_KDEplots.py
@@ -0,0 +1,109 @@
+﻿#%% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
+import os
+try:
+	os.chdir(os.path.join(os.getcwd(), 'ipy'))
+	print(os.getcwd())
+except:
+	pass
+#%% [markdown]
+# # [EDA] 了解變數分布狀態: Bar & KDE (density plot)
+#%% [markdown]
+# # [教學目標]
+# - 以下程式碼將示範如何繪製各種分布圖, 更直覺地看出單一變數本身的分布情形
+# - 分布圖包含 KDE(Kenel Density Estimation) 曲線, 以及 Bar(區間長條圖), 提供兩種不同感受
+# - 順便熟悉 matplotlib 的幾種繪圖設定
+#%% [markdown]
+# # [範例重點]
+# - 各種樣式的長條圖(Bar) (In[4], Out[4])   
+# - 不同的 KDE 曲線與繪圖設定 (In[5], Out[5]) 以及切換不同 Kernel function 的效果 (In[6], Out[6])
+
+#%%
+# 載入需要的套件
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns # 另一個繪圖-樣式套件
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+# 忽略警告訊息
+import warnings
+warnings.filterwarnings('ignore')
+
+# 設定 data_path
+dir_data = './data/'
+
+
+#%%
+# 讀取檔案
+f_app = os.path.join(dir_data, 'application_train.csv')
+print('Path of read in data: %s' % (f_app))
+app_train = pd.read_csv(f_app)
+app_train.head()
+
+
+#%%
+# 資料整理 ( 'DAYS_BIRTH'全部取絕對值 )
+app_train['DAYS_BIRTH'] = abs(app_train['DAYS_BIRTH'])
+
+
+#%%
+# 以生存年數繪製分布圖
+plt.hist(app_train['DAYS_BIRTH'] / 365, edgecolor = 'k', bins = 25)
+plt.title('Age of Client'); plt.xlabel('Age (years)'); plt.ylabel('Count');
+plt.show()
+
+# 改變繪圖樣式 (style)
+plt.style.use('dark_background') 
+
+# 改變樣式後再繪圖一次, 比較效果
+plt.hist(app_train['DAYS_BIRTH'] / 365, edgecolor = 'k', bins = 25)
+plt.title('Age of Client'); plt.xlabel('Age (years)'); plt.ylabel('Count');
+plt.show()
+
+
+#%%
+# 設定繪圖區域的長與寬
+plt.figure(figsize = (10, 8))
+
+# Kenel Density Estimation (KDE) plot: 會準時還貸者 - 下圖紅線
+sns.kdeplot(app_train.loc[app_train['TARGET'] == 0, 'DAYS_BIRTH'] / 365, label = 'target == 0')
+# KDE plot: 不會準時還貸者 - 下圖藍線
+sns.kdeplot(app_train.loc[app_train['TARGET'] == 1, 'DAYS_BIRTH'] / 365, label = 'target == 1')
+
+# 設定標題與 X, y 軸的說明
+plt.xlabel('Age (years)'); plt.ylabel('Density'); plt.title('Distribution of Ages');
+
+# 輸出繪圖 : 有的ipynb環境, 會在一格結束時自動輸出繪圖, 但加上這行能保證在不同環境都會畫出圖形
+plt.show()
+
+
+#%%
+# KDE, 比較不同的 kernel function
+sns.kdeplot(app_train.loc[app_train['TARGET'] == 0, 'DAYS_BIRTH'] / 365, label = 'Gaussian esti.', kernel='gau')
+sns.kdeplot(app_train.loc[app_train['TARGET'] == 0, 'DAYS_BIRTH'] / 365, label = 'Cosine esti.', kernel='cos')
+sns.kdeplot(app_train.loc[app_train['TARGET'] == 0, 'DAYS_BIRTH'] / 365, label = 'Triangular esti.', kernel='tri')
+plt.show()
+
+#%% [markdown]
+# ### 補充說明: 
+# 1. [KDE](https://scikit-learn.org/stable/auto_examples/neighbors/plot_kde_1d.html#sphx-glr-auto-examples-neighbors-plot-kde-1d-py)
+# 2. [什麼是 KDE](https://blog.csdn.net/unixtch/article/details/78556499)
+
+#%%
+# 完整分布圖 (distplot) : 將 bar 與 Kde 同時呈現
+sns.distplot(app_train.loc[app_train['TARGET'] == 1, 'DAYS_BIRTH'] / 365, label = 'target == 1')
+plt.legend()
+plt.xlabel('Age (years)'); plt.ylabel('Density'); plt.title('Distribution of Ages');
+plt.show()
+
+#%% [markdown]
+# # It's your turn
+# ## 除了直接觀察變項以外，我們也可以分群比較
+# 1. 自 20 到 70 歲，切 11 個點，進行分群比較 (KDE plot)
+# 2. 以年齡區間為 x, target 為 y 繪製 barplot
+
+#%%
+
+
+
---
+++ py/Day_011_HW.py
@@ -0,0 +1,116 @@
+﻿#%% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
+import os
+try:
+	os.chdir(os.path.join(os.getcwd(), 'ipy'))
+	print(os.getcwd())
+except:
+	pass
+#%% [markdown]
+# # [EDA] 了解變數分布狀態: Bar & KDE (density plot)
+#%% [markdown]
+# # To do: 變項的分群比較
+# 1. 自 20 到 70 歲，切 11 個點，進行分群比較 (KDE plot)
+# 2. 以年齡區間為 x, target 為 y 繪製 barplot
+#%% [markdown]
+# # [作業目標]
+# - 試著調整資料, 並利用提供的程式繪製分布圖
+#%% [markdown]
+# # [作業重點]
+# - 如何將資料依照歲數, 將 20 到 70 歲切成11個區間? (In[4], Hint : 使用 numpy.linspace),  
+#   送入繪圖前的除了排序外, 還要注意什麼? (In[5])
+# - 如何調整對應資料, 以繪製長條圖(bar chart)? (In[7])
+
+#%%
+# 載入需要的套件
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns # 另一個繪圖-樣式套件
+get_ipython().run_line_magic('matplotlib', 'inline')
+plt.style.use('dark_background')
+
+# 忽略警告訊息
+import warnings
+warnings.filterwarnings('ignore')
+
+# 設定 data_path
+dir_data = './data/'
+
+
+#%%
+# 讀取檔案
+print(os.getcwd())
+f_app = os.path.join(dir_data, 'application_train.csv')
+print('Path of read in data: %s' % (f_app))
+app_train = pd.read_csv(f_app)
+app_train.head()
+
+
+#%%
+# 資料整理 ( 'DAYS_BIRTH'全部取絕對值 )
+app_train['DAYS_BIRTH'] = abs(app_train['DAYS_BIRTH'])
+
+
+#%%
+# 根據年齡分成不同組別 (年齡區間 - 還款與否)
+age_data = app_train[['TARGET', 'DAYS_BIRTH']] # subset
+age_data['YEARS_BIRTH'] = age_data['DAYS_BIRTH'] / 365 # day-age to year-age
+
+#自 20 到 70 歲，切 11 個點 (得到 10 組)
+# """
+# Your Code Here
+#  """
+bin_cut =  [ 20+(50/11)*i for i in range(12)]
+age_data['YEARS_BINNED'] = pd.cut(age_data['YEARS_BIRTH'], bins = bin_cut) 
+
+# 顯示不同組的數量
+print(age_data['YEARS_BINNED'].value_counts())
+age_data.head()
+
+
+#%%
+year_group_sorted = age_data.sort_values('YEARS_BINNED')['YEARS_BINNED'].unique()
+print(year_group_sorted)
+#%%
+# 繪圖前先排序 / 分組
+"""
+Your Code Here
+"""
+
+year_group_sorted = age_data.sort_values('YEARS_BINNED')['YEARS_BINNED'].unique()
+
+plt.figure(figsize=(8,6))
+for i in range(len(year_group_sorted)):
+    sns.distplot(age_data.loc[(age_data['YEARS_BINNED'] == year_group_sorted[i]) &(age_data['TARGET'] == 0), 'YEARS_BIRTH'], label = str(year_group_sorted[i]))
+    
+    sns.distplot(age_data.loc[(age_data['YEARS_BINNED'] == year_group_sorted[i]) & (age_data['TARGET'] == 1), 'YEARS_BIRTH'], label = str(year_group_sorted[i]))
+plt.title('KDE with Age groups')
+plt.show()
+
+
+#%%
+# 計算每個年齡區間的 Target、DAYS_BIRTH與 YEARS_BIRTH 的平均值
+age_groups  = age_data.groupby('YEARS_BINNED').mean()
+age_groups
+
+
+#%%
+plt.figure(figsize = (8, 8))
+
+# 以年齡區間為 x, target 為 y 繪製 barplot
+"""
+Your Code Here
+"""
+px = list(age_data['YEARS_BINNED'].unique())
+py = age_groups['TARGET']
+sns.barplot(px, py)
+
+# Plot labeling
+plt.xticks(rotation = 75); plt.xlabel('Age Group (years)'); plt.ylabel('Failure to Repay (%)')
+plt.title('Failure to Repay by Age Group');
+
+
+
+
+#%%
