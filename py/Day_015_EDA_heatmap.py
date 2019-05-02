36690cc7057806f2b06afc2e3d8803846184d936
Home Windows <acetaxxxx@gmail.com>
2019-05-02 23:01:40 +0800

    Day15

---
+++ py/Day_015_EDA_heatmap.py
@@ -0,0 +1,147 @@
+﻿#%% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
+import os
+try:
+	os.chdir(os.path.join(os.getcwd(), 'ipy'))
+	print(os.getcwd())
+except:
+	pass
+#%% [markdown]
+# # [EDA] Heatmap & Grid-scatters
+# 可以有效的將表格型的圖進行視覺化, 常見的 Heatmap
+# * 相關圖
+# * Confusion matrix (混淆矩陣)
+# 
+#%% [markdown]
+# # [教學目標]
+# - 在 EDA 中, 活用各種 Heatmap
+#%% [markdown]
+# # [範例重點]
+# - Heatmap 的基礎用法 : 相關矩陣的 Heatmap (In[4], Out[4])   
+# - Heatmap 的進階用法 : 散佈圖, KDE, 密度圖 (In[6], Out[6], In[7], Out[7])
+
+#%%
+# 載入需要的套件
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns # 另一個繪圖-樣式套件
+plt.style.use('ggplot')
+
+# 忽略警告訊息
+get_ipython().run_line_magic('matplotlib', 'inline')
+import warnings
+warnings.filterwarnings('ignore')
+
+# 設定 data_path
+dir_data = './data/'
+
+
+#%%
+# 讀取檔案
+f_app = os.path.join(dir_data, 'application_train.csv')
+print('Path of read in data: %s' % (f_app))
+app_train = pd.read_csv(f_app)
+app_train.head()
+
+
+#%%
+# 取出 EXT_SOURCE 的幾項變數並檢驗其相關性
+app_train['DAYS_BIRTH'] = abs(app_train['DAYS_BIRTH'])
+ext_data = app_train[['TARGET', 'EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3', 'DAYS_BIRTH']]
+ext_data_corrs = ext_data.corr()
+ext_data_corrs
+
+
+#%%
+plt.figure(figsize = (8, 6))
+# 繪製相關係數 (correlations) 的 Heatmap
+sns.heatmap(ext_data_corrs, cmap = plt.cm.RdYlBu_r, vmin = -0.25, annot = True, vmax = 0.6)
+plt.title('Correlation Heatmap');
+
+
+#%%
+# 進一步我們檢查這三項變數在 Target 上的分布是否不同
+plt.figure(figsize = (24, 8))
+
+# 依不同 EXT_SOURCE 逐項繪製 KDE 圖形
+for i, source in enumerate(['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3']):
+    # 做 subplot
+    plt.subplot(1, 3, i + 1)
+    
+    # KDE 圖形
+    sns.kdeplot(app_train.loc[app_train['TARGET'] == 0, source], label = 'target == 0')
+    sns.kdeplot(app_train.loc[app_train['TARGET'] == 1, source], label = 'target == 1')
+    
+    # 加上各式圖形標籤
+    plt.title('Distribution of %s by Target Value' % source)
+    plt.xlabel('%s' % source); plt.ylabel('Density');
+    
+plt.tight_layout(h_pad = 2.5)
+
+
+#%%
+# 轉成以年記齡後，將以日記齡丟掉
+plot_data = ext_data.copy()
+plot_data['YEARS_BIRTH'] = plot_data['DAYS_BIRTH'] / 365
+plot_data.drop(['DAYS_BIRTH'],axis = 1, inplace=True)
+
+# 定義函數 : 計算兩個 column 之間的相關係數
+def corr_func(x, y, **kwargs):
+    r = np.corrcoef(x, y)[0][1]
+    ax = plt.gca()
+    ax.annotate("r = {:.2f}".format(r),
+                xy=(.2, .8), xycoords=ax.transAxes,
+                size = 20)
+
+
+#%%
+N_sample = 100000
+# 把 NaN 數值刪去, 並限制資料上限為 100000 : 因為要畫點圖, 如果點太多，會畫很久!
+plot_data = plot_data.dropna().sample(n = N_sample)
+# 建立 pairgrid 物件
+grid = sns.PairGrid(data = plot_data, size = 3, diag_sharey=False,
+                    hue = 'TARGET', 
+                    vars = [x for x in list(plot_data.columns) if x != 'TARGET'])
+
+# 上半部為 scatter
+grid.map_upper(plt.scatter, alpha = 0.2)
+
+# 對角線畫 histogram
+grid.map_diag(sns.kdeplot)
+
+# 下半部放 density plot
+grid.map_lower(sns.kdeplot, cmap = plt.cm.OrRd_r)
+
+plt.suptitle('Ext Source and Age Features Pairs Plot', size = 32, y = 1.05)
+plt.show()
+
+
+#%%
+# 試著以不同 size 畫看看，觀察有那裡不太一樣
+N_sample = 1000
+
+plot_data = plot_data.dropna().sample(n = N_sample)
+# 建立 pairgrid 物件
+grid = sns.PairGrid(data = plot_data, size = 3, diag_sharey=False,
+                    hue = 'TARGET', 
+                    vars = [x for x in list(plot_data.columns) if x != 'TARGET'])
+
+# 上半部為 scatter
+grid.map_upper(plt.scatter, alpha = 0.2)
+
+# 對角線畫 histogram
+grid.map_diag(sns.kdeplot)
+
+# 下半部放 density plot
+grid.map_lower(sns.kdeplot, cmap = plt.cm.OrRd_r)
+
+plt.suptitle('Ext Source and Age Features Pairs Plot', size = 32, y = 1.05)
+plt.show()
+
+#%% [markdown]
+# ## 作業
+# 1. 請用 numpy 建立一個 10 x 10, 數值分布自 -1.0 ~ 1.0 的矩陣並繪製 Heatmap
+# 2. 請用 numpy 建立一個 1000 x 3, 數值分布為 -1.0 ~ 1.0 的矩陣，並繪製 PairPlot (上半部為 scatter, 對角線為 hist, 下半部為 density)
+# 3. 請用 numpy 建立一個 1000 x 3, 數值分布為常態分佈的矩陣，並繪製 PairPlot (上半部為 scatter, 對角線為 hist, 下半部為 density)
+
---
+++ py/Day_015_HW.py
@@ -0,0 +1,106 @@
+﻿#%% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
+import os
+try:
+	os.chdir(os.path.join(os.getcwd(), 'ipy'))
+	print(os.getcwd())
+except:
+	pass
+#%% [markdown]
+# ## 作業
+# 1. 請用 numpy 建立一個 10 x 10, 數值分布自 -1.0 ~ 1.0 的矩陣並繪製 Heatmap
+# 2. 請用 numpy 建立一個 1000 x 3, 數值分布為 -1.0 ~ 1.0 的矩陣，並繪製 PairPlot (上半部為 scatter, 對角線為 hist, 下半部為 density)
+# 3. 請用 numpy 建立一個 1000 x 3, 數值分布為常態分佈的矩陣，並繪製 PairPlot (上半部為 scatter, 對角線為 hist, 下半部為 density)
+#%% [markdown]
+# # [作業目標]
+# - 試著設定隨機資料, 並依照範例練習基礎與進階的 Heatmap
+#%% [markdown]
+# # [作業重點]
+# - 如題1.條件隨機矩陣, 並仿造基礎 Heatmap 範例作圖   
+# (In[2], OUT[2]) (Hint : numpy.random.random - 均勻分布, 隨機小數)
+# - 如題2.3.條件隨機數值列, 並仿造進階 Heatmap 範例作圖  
+# (In[3], OUT[3], In[4], OUT[4]) (Hint : numpy.random.randn - 常態分布)
+
+#%%
+# 載入需要的套件
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns # 另一個繪圖-樣式套件
+plt.style.use('ggplot')
+
+# 忽略警告訊息
+get_ipython().run_line_magic('matplotlib', 'inline')
+import warnings
+warnings.filterwarnings('ignore')
+
+
+#%%
+"""
+Your Code Here
+"""
+matrix = 
+
+plt.figure(figsize=(10,10))
+"""
+Your Code Here
+"""
+heatmap = 
+
+plt.show()
+
+
+#%%
+nrow = 1000
+ncol = 3
+"""
+Your Code Here
+"""
+matrix = 
+
+# 隨機給予 0, 1, 2 三種標籤
+indice = np.random.choice([0,1,2], size=nrow)
+plot_data = pd.DataFrame(matrix, indice)
+
+# 繪製 seborn 進階 Heatmap
+grid = sns.PairGrid(data = plot_data, size = 3, diag_sharey=False)
+"""
+Your Code Here
+Please replace "..." to correct plot function
+"""
+grid.map_upper(... , alpha = 0.2)
+grid.map_diag(... )
+grid.map_lower(... , cmap = plt.cm.OrRd_r)
+
+plt.show()
+
+
+#%%
+nrow = 1000
+ncol = 3
+"""
+Your Code Here
+"""
+matrix = 
+
+# 隨機給予 0, 1, 2 三種標籤
+indice = np.random.choice([0,1,2], size=nrow)
+plot_data = pd.DataFrame(matrix, indice)
+
+# 繪製 seborn 進階 Heatmap
+grid = sns.PairGrid(data = plot_data, size = 3, diag_sharey=False)
+"""
+Your Code Here
+Please replace "..." to correct plot function
+"""
+grid.map_upper(... , alpha = 0.2)
+grid.map_diag(... )
+grid.map_lower(... , cmap = plt.cm.OrRd_r)
+
+plt.show()
+
+
+#%%
+
+
+
